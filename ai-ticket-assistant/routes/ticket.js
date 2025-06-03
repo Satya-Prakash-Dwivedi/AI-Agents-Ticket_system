0f3c93569a21bc5af92222f11352ca47a8289b4c
Satya-Prakash-Dwivedi <dwivediprakashsatya36@gmail.com>
2025-06-03 13:21:05 +0530

    Build ticket contoller and inngest middleware

--- ai-ticket-assistant/routes/ticket.js
+++ ai-ticket-assistant/routes/ticket.js
@@ -1,5 +1,11 @@
 import express from "express"
+import { createTicket, getTicket, getTickets } from "../controllers/ticket"
+import { authenticate } from "../middlewares/auth";
 
 const router = express.Router();
+
+router.get("/", authenticate, getTickets);
+router.get("/:id", authenticate, getTicket);
+router.post("/", authenticate, createTicket);
 
 export default router;
