dffa397e2a08afe57b87be536453e60c636f761f
Satya-Prakash-Dwivedi <dwivediprakashsatya36@gmail.com>
2025-06-03 09:21:53 +0530

    Build an AI-Agent Pipeline

---
+++ ai-ticket-assistant/inngest/functions/on-ticket-create.js
@@ -0,0 +1,87 @@
+import { inngest } from "../client";
+import Ticket from "../../models/ticket"
+import User from "../../models/user";
+import { NonRetriableError } from "inngest";
+import { sendMail } from "../../utils/mailer";
+import analyzeTicket from "../../utils/ai";
+
+export const onTicketCreated = inngest.createFunction(
+    { id: "on-ticket-created", retries: 2 },
+    { event: "ticket/created" },
+    async({event, step}) => {
+        try {
+            
+            const { ticketId } = event.data;
+
+            // Fetch ticket from DB
+            const ticket = await step.run("fetch-ticket", async() => {
+                const ticketObject = await Ticket.findById(ticketId);
+                if(!ticket){
+                    throw new NonRetriableError("Ticket not found");
+                }
+                return ticketObject;
+            });
+
+            await step.run("update-ticket-status", async() => {
+                await Ticket.findByIdAndUpdate(ticket._id, { status: "TODO" });
+            });
+
+            const aiResponse = await analyzeTicket(ticket);
+
+            const relatedskills = await step.run("ai-processing", async () => {
+                let skills = [];
+                if (aiResponse){
+                    await Ticket.findByIdAndUpdate(ticket._id, {
+                        priority: !["low", "medium", "high"].includes(aiResponse.priority)
+                        ? "medium"
+                        : aiResponse.priority,
+                        helpfulNotes: aiResponse.helpfulNotes,
+                        status: "IN_PROGRESS",
+                        relatedSkills: aiResponse.relatedSkills,
+                    });
+                    skills = aiResponse.relatedSkills;
+                }
+                return skills;
+            });
+
+            const moderator = await step.run("assign-moderator", async () => {
+                let user = await User.findOne({
+                    role: "moderator",
+                    skills: {
+                        $eleMatch: {
+                            $regex: relatedskills.join("|"),
+                            $options: "i",
+                        },
+                    },
+                });
+
+                if(!user){
+                    user = await User.findOne({
+                        role: "admin",
+                    });
+                }
+                await Ticket.findByIdAndUpdate(ticket._id,{
+                    assignedTo: user?._id || null,
+                });
+                return user;
+            });
+
+            await step.run("send-email-notification", async () => {
+                if(moderator){
+                    const finalTicket = await Ticket.findById(ticket._id);
+                    await sendMail(
+                        moderator.email,
+                        "Ticket assigned",
+                        `A new ticket is assigned to you ${finalTicket.title}`
+                    );
+                }
+            });
+
+            return { success: true }
+        } catch (err) {
+            console.error("❌ Error running the step", err.message);
+            return { success: false };
+        }
+    }
+)
+
